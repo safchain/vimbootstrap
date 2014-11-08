0f9531eca0c6561f882f18ec579fb5c540f42a6b
Sylvain Afchain <safchain@gmail.com>
2014-11-08 12:37:27 +0000

    From bitbucket to github

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+DIR=`pwd`
+cd ~/.vim/bundle
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/klen/python-mode.git
+git clone https://github.com/Lokaltog/vim-powerline.git
+git clone git://github.com/tpope/vim-fugitive.git
+git clone git://github.com/airblade/vim-gitgutter.git
+cd $DIR
+
+mkdir ~/.vim/colors && \
+    curl -LSso ~/.vim/colors/sacolors.vim \
+        https://raw.githubusercontent.com/safchain/vimbootstrap/master/sacolors.vim
+
+cp .vimrc ~/
